3b10e36a5e884a847139929bf7e4a9208919f3db
Levente <szebik.levente@gmail.com>
2024-04-16 16:49:29 +0200

    bugfix

--- train.py
+++ train.py
@@ -13,15 +13,30 @@ from keras.layers import Flatten
 from keras.optimizers import SGD
 from sklearn.metrics import accuracy_score
 
-DATADIR = "E:\\new_sodoku\\images\\"
+DATADIR = "E:\\sodoku\\images\\"
 CATEGORIES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 IMG_SIZE = 128
 
-x = []
-y = []
 training_data = []
 
-for features,label in training_data:
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([img_array, class_num])
+            except Exception as e:
+                pass
+
+create_training_data()
+
+x = []
+y = []
+
+for features, label in training_data:
     x.append(features)
     y.append(label)
 
@@ -42,22 +57,9 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x, y, epochs = 5)
+model.fit(x, y, epochs=5)
 
 model.save('model.h5')
-
-def create_training_data():
-    for category in CATEGORIES:
-        path = os.path.join(DATADIR,category)
-        class_num = CATEGORIES.index(category)
-        for img in os.listdir(path):
-            try:
-                img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
-                training_data.append([img_array, class_num])
-            except Exception as e:
-                pass
-
-create_training_data()
 
 print("***********TRAINING***********")
 print("###Images processed: " + str(len(training_data)) + " ###")
