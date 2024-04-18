0121808c162a6f01fd164bfa71a7749936a9d938
Levente <szebik.levente@gmail.com>
2024-04-18 10:27:55 +0200

    train, and digit detection

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import cv2
+import cv2 #import the library 
 import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+import tensorflow as tf
+from statistics import *
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import img_to_array
 
 stream_url = 'https://192.168.1.65:8080/video'
 cap = cv2.VideoCapture(stream_url)
@@ -7,14 +19,19 @@ cap = cv2.VideoCapture(stream_url)
 cap.set(3, 640)
 cap.set(4, 480)
 
-def getContours(img, original_img):
-    contours,hierachy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+def getContours(img,original_img):
+    
+    contours,hierachy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
+        
         area = cv2.contourArea(cnt)
-        if area > 90000:
-            cv2.drawContours(original_img, cnt, -1, (0, 255, 0), 3)
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+       
+        if area > 60000:
+            cv2.drawContours(original_img,cnt,-1,(0,255,0),2)
+            #print(area)
+            
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
             ax = approx.item(0)
             ay = approx.item(1)
             bx = approx.item(2)
@@ -23,37 +40,207 @@ def getContours(img, original_img):
             cy = approx.item(5)
             dx = approx.item(6)
             dy = approx.item(7)
+            
+            width,height= 900,900
 
-            width,height = 900,900
+            pts1 = np.float32([[bx,by],[ax,ay],[cx,cy],[dx,dy]])
+            pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 
-            pts1 = np.float32([[bx, by], [ax, ay], [cx, cy], [dx, dy]])
-            pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
-
-            matrix = cv2.getPerspectiveTransform(pts1, pts2)
-            img_perspective = cv2.warpPerspective(original_img, matrix, (width, height))
-            img_corners = cv2.cvtColor(img_perspective, cv2.COLOR_BGR2GRAY)
-
+            matrix = cv2.getPerspectiveTransform(pts1,pts2)
+            img_perspective = cv2.warpPerspective(original_img,matrix,(width,height))
+            img_corners = cv2.cvtColor(img_perspective,cv2.COLOR_BGR2GRAY)
+            
+            # Turn into a binary image
             for x in range(0, 900):
                 for y in range(0, 900):
-                    if img_corners[x][y] < 100:
-                        img_corners[x][y] = 0
+                    if img_corners[x][y]<100:
+                        img_corners[x][y]=0
                     else:
-                        img_corners[x][y] = 255
+                        img_corners[x][y]=255
+                        
+            cv2.imshow('Corners',img_corners)
+            
+            return img_corners
+            
+            #classify(img_corners)
 
-            # return img_contours
-            cv2.imshow('Corners', img_contours)
+def classify(Img):
+     
+    crop_val = 20
+    digits_list = []
+
+    
+    
+    for i in range(0, 9):
+        for j in range(0, 9):
+            
+                
+            J = j+1
+            I = i+1
+            cell = Img[I*100 - 100 + crop_val : I*100 - crop_val , J*100 - 100 + crop_val : J*100 - crop_val]
+
+
+            img_canny = cv2.Canny(cell, 50, 50)
+            # plt.imshow(img_canny, cmap='gray')
+            # plt.show()
+
+            contours, hierachy = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            print(contours)
+            
+            
+            
+            digit = 0
+            prob = 1.0
+
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+
+                if area > 7.5:
+
+                    peri = cv2.arcLength(cnt, True)
+                    approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+
+                    x,y,w,h = cv2.boundingRect(approx)
+
+                    image_rect = cell[y:y+h, x:x+w]
+                    image_rect = cv2.resize(image_rect, (100, 100))
+
+                    image_num = img_to_array(image_rect)
+
+                    image_num = np.array(image_num).reshape(-1, 100 , 100,1)
+                    image_num = image_num.astype('float32')
+                    image_num = image_num / 255.0
+
+                    model = tf.keras.models.load_model('model.h5')
+                    prediction = model.predict(image_num)
+                    digit = int(np.argmax(prediction))
+                    prob = np.amax(prediction)
+                    plt.imshow(image_rect, cmap='gray')
+                    plt.show()
+            print("Detected: ",digit)
+            print("Probability: ",prob)
+            digits_list.append(digit)
+    return digits_list
+
+def is_valid(grid, num, coordinate):
+    # Check row
+    for i in range(len(grid[0])):
+        if grid[coordinate[0]][i] == num and coordinate[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(grid)):
+        if grid[i][coordinate[1]] == num and coordinate[0] != i:
+            return False
+
+    # Check box
+    box_x = coordinate[1] // 3
+    box_y = coordinate[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if grid[i][j] == num and (i,j) != coordinate:
+                return False
+
+    return True
+
+def solve(grid):
+    find = find_empty(grid)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if is_valid(grid, i, (row, col)):
+            grid[row][col] = i
+
+            if solve(grid):
+                return True
+
+            grid[row][col] = 0
+        
+
+    return False
+
+
+
+def find_empty(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+def save_sudoku(sudoku2d, sudoku2d_unsolved):
+    
+    solved_cell = np.ones((900, 900, 3))
+    # Draw the sublines
+    for i in range(8):
+        solved_cell = cv2.line(solved_cell, ((i+1)*100, 0), ((i+1)*100, 900), (255, 255, 255), 5)
+        solved_cell = cv2.line(solved_cell, (0,(i+1)*100), (900, (i+1)*100), (255, 255, 255), 5)
+    # Draw the lines
+    for i in range(2):
+        solved_cell = cv2.line(solved_cell, ((i+1)*300, 0), ((i+1)*300, 900), (255, 255, 255), 10)
+        solved_cell = cv2.line(solved_cell, (0,(i+1)*300), (900, (i+1)*300), (255, 255, 255), 10)
+
+
+
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 2
+    thickness = 4
+    for (index_row, row) in enumerate(sudoku2d):
+        for (index_num, num) in enumerate(row):
+            pos = (index_num*100 + 25, index_row*100 + 70)
+            color = (200, 200, 200)
+            if sudoku2d_unsolved[index_row][index_num] == 0:
+                color = (0, 200, 0)
+            
+            solved_cell = cv2.putText(solved_cell ,str(num), pos, font, 
+                           fontScale, color, thickness, cv2.LINE_AA)
+
+    cv2.imwrite('solved.png', solved_cell)
+    cv2.imshow('Solved',solved_cell)
+
+grid_found = False
+grid_img = None
 
 while True:
     success, img = cap.read()
-
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.GaussianBlur(img_gray, (5,5), 3)
-    img_canny = cv2.Canny(img_blur, 50, 50)
     img_copy = img.copy()
-
-    getContours(img_canny, img_copy)
-
-    cv2.imshow('Sodoku Solver', img_copy)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        cv2.destroyAllWindows()
+    cv2.imshow('Sudoku Solver', img_copy)
+    if not grid_found:
+        # Turning the original image to canny.
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        imgBlur = cv2.GaussianBlur(imgGray,(5,5),3)
+        imgCanny = cv2.Canny(imgBlur,50,50)
+        img_contours_bin = getContours(imgCanny, img_copy)
+        if img_contours_bin is not None:
+            grid_found = True
+            grid_img = img_contours_bin
+            sudoku = classify(img_contours_bin)
+            sudoku2d = [sudoku[i*9:(i+1)*9] for i in range(9)]
+            sudoku2d = np.array(sudoku2d)
+            sudoku2d_unsolved = sudoku2d.copy()
+            print(sudoku2d_unsolved)
+            solve(sudoku2d)
+            save_sudoku(sudoku2d, sudoku2d_unsolved)
+            break
+    
+    else:
+        if grid_img is not None:
+            try:
+                # Display the ongoing solving process
+                cv2.imshow('Sudoku Solver', grid_img)
+            except Exception as e:
+                print(f"Error during grid processing: {e}")
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    
+    
+    
+cv2.destroyAllWindows()
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 import tensorflow as tf
+from tqdm import tqdm
 
 from sklearn.model_selection import KFold
 from statistics import *
@@ -15,34 +16,114 @@ from sklearn.metrics import accuracy_score
 
 DATADIR = "E:\\sodoku\\images\\"
 CATEGORIES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-IMG_SIZE = 128
+
 
 training_data = []
 
-def create_training_data():
-    for category in CATEGORIES:
-        path = os.path.join(DATADIR, category)
-        class_num = CATEGORIES.index(category)
-        for img in os.listdir(path):
+
+# def create_training_data():
+#     for category in CATEGORIES:
+#         path = os.path.join(DATADIR, category)
+#         class_num = CATEGORIES.index(category)
+#         for img in tqdm(os.listdir(path), desc=f"Processing {category}"):  # Use tqdm here
+#             try:
+#                 img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+#                 img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+#                 training_data.append([img_array, class_num])
+#             except Exception as e:
+#                 pass
+
+def data_augmentation():
+    for category in CATEGORIES:  
+
+        path = os.path.join(DATADIR,category)  
+        class_num = CATEGORIES.index(category)  # get the classification
+
+        for img_filename in (os.listdir(path)):  # iterate over each image per digits
             try:
-                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-                img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-                training_data.append([img_array, class_num])
-            except Exception as e:
-                pass
+                
+                img_array = cv2.imread(os.path.join(path,img_filename),cv2.IMREAD_GRAYSCALE)
+                #plt.imshow(img_array, cmap='gray')
+                #plt.show()
+                
+                img_canny = cv2.Canny(img_array, 50, 50)
+                
+                contours, hierachy = cv2.findContours(img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                
+                for cnt in contours:
+                    area = cv2.contourArea(cnt)
+                    
+                    if area > 5:
+                        
+                        peri = cv2.arcLength(cnt, True)
+                        approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
+                        
+                        x,y,w,h = cv2.boundingRect(approx)
+                        
+                        image_rect = img_array[y:y+h, x:x+w]
+                        image_rect = cv2.resize(image_rect, (100, 100))
+                        #plt.imshow(image_rect, cmap='gray')
+                        #plt.show()
+                        
+                        kernel = np.ones((5,5), np.uint8)
+                        
+                        for blur_value in range(-30, 30):
+                            
+                            img = cv2.GaussianBlur(image_rect, (7,7), blur_value)
+                            training_data.append([img, class_num])
+                            
+                            
+                            
+                            img_erosion = cv2.erode(img, kernel, iterations = 1)
+                            img_erosion2 = cv2.erode(img, kernel, iterations = 2)
+                            training_data.append([img_erosion, class_num])
+                            training_data.append([img_erosion2, class_num])
+                            
+                            
+                            
+                            
+                            img_dilation = cv2.dilate(img, kernel, iterations = 1)
+                            img_dilation2 = cv2.dilate(img, kernel, iterations = 2)
+                            training_data.append([img_dilation, class_num])
+                            training_data.append([img_dilation2, class_num])
+                            
+                            #plt.imshow(img_dilation2, cmap='gray')
+                            #plt.show()
+                            
+                        
+                        
+                        
+                
+                    
+                
+            except Exception as e:  # in the interest in keeping the output clean...
+                raise(e)
+            
 
-create_training_data()
+data_augmentation()
 
-x = []
+print(len(training_data))
+
+import random
+random.seed(2700)
+random.shuffle(training_data)
+
+
+for features,label in training_data[:1]:
+    print(features) 
+
+IMG_SIZE = 100
+
+X = []
 y = []
 
-for features, label in training_data:
-    x.append(features)
+for features,label in training_data:
+    X.append(features)
     y.append(label)
-
-x = np.array(x).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-x = x.astype('float32')
-x = x / 255.0
+    
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+X = X.astype('float32')
+X = X / 255.0
 
 y = np.array(y)
 
@@ -53,11 +134,12 @@ model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
+
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x, y, epochs=5)
+model.fit(X, y, epochs = 3)
 
 model.save('model.h5')
 
